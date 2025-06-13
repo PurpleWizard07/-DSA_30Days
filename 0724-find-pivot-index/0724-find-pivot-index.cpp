611cde69b41cbddc3f15a4de2cb0c04aac134d4a
PurpleWizard07 <purplewizard0709@gmail.com>
2025-06-13 18:38:12 +0530

    Time: 0 ms (100%), Space: 35.5 MB (95.04%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,18 +2,40 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-        vector<int> left(n);
-        vector<int> right(n);
-        left[0] = nums[0], right[n-1] = nums[n-1];
-        for(int i=1; i<n; i++) {
-            left[i] = left[i-1] + nums[i];
+        int sum = 0;
+        for(auto num: nums) {
+            sum += num;
         }
-        for(int i=n-2; i>=0; i--) {
-            right[i] = right[i+1] + nums[i];
-        }
+        int left_sum = 0;
+        int right_sum = sum;
         for(int i=0; i<n; i++) {
-            if(left[i] == right[i]) return i;
+            right_sum -= nums[i];
+            if(left_sum == right_sum) return i;
+            left_sum += nums[i];
         }
         return -1;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> left(n);
+//         vector<int> right(n);
+//         left[0] = nums[0], right[n-1] = nums[n-1];
+//         for(int i=1; i<n; i++) {
+//             left[i] = left[i-1] + nums[i];
+//         }
+//         for(int i=n-2; i>=0; i--) {
+//             right[i] = right[i+1] + nums[i];
+//         }
+//         for(int i=0; i<n; i++) {
+//             if(left[i] == right[i]) return i;
+//         }
+//         return -1;
+//     }
+// };
