a66eea824ca15d102c44bffd9bf739a7af6d86ac
PurpleWizard07 <purplewizard0709@gmail.com>
2025-06-13 12:55:20 +0530

    Time: 0 ms (100%), Space: 63 MB (48.52%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left = 0, right = height.size() - 1;
-        int maxi = 0;
-        
-        while (left < right) {
-            // Use long long to prevent overflow
-            long long area = min(height[left], height[right]) * (right - left);
-            maxi = max(maxi, (int)area);
-            
-            // Move the pointer pointing to the shorter line
-            if (height[left] < height[right]) left++;
+        int left = 0, right = height.size() - 1, maxi = 0;
+        while(left < right) {
+            int area = min(height[left], height[right]) * (right-left);
+            maxi = max(maxi, area);
+            if(height[left] < height[right]) left++;
             else right--;
         }
-        
         return maxi;
     }
 };
