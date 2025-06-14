cbd441b06ceb9dd1ae04862f86bbebba0e3b18dd
PurpleWizard07 <purplewizard0709@gmail.com>
2025-06-15 02:42:50 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.1%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long ans = 0;
-        for (long long i = 1; i * i <= x; i++) {
-            ans = i;
-        }
-        return ans;
+      if(x==0) return 0;
+      long long r = x;
+      while(r*r > x) {
+        r = (r + x/r) / 2;
+      }return r;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         long ans = 0;
+//         for (long long i = 1; i * i <= x; i++) {
+//             ans = i;
+//         }
+//         return ans;
+//     }
+// };
