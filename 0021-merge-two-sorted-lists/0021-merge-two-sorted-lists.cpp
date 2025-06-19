3040367c83658eb3812ed24581cb016abed54d9d
PurpleWizard07 <purplewizard0709@gmail.com>
2025-06-19 12:49:45 +0530

    Time: 0 ms (100%), Space: 19.5 MB (29.42%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,18 +10,41 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL || list2 == NULL) {
-            return list1 == NULL ? list2 : list1;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy;
+        ListNode* head = &dummy;
+        while(l1 != nullptr && l2 != nullptr) {
+            if(l1->val < l2->val) {
+                head->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
         }
-
-        if(list1->val <= list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        } 
-        else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
-        }
+        head->next = (l1 != nullptr) ? l1 : l2;
+        return dummy.next;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1 == NULL || list2 == NULL) {
+//             return list1 == NULL ? list2 : list1;
+//         }
+
+//         if(list1->val <= list2->val) {
+//             list1->next = mergeTwoLists(list1->next, list2);
+//             return list1;
+//         } 
+//         else {
+//             list2->next = mergeTwoLists(list1, list2->next);
+//             return list2;
+//         }
+//     }
+// };
