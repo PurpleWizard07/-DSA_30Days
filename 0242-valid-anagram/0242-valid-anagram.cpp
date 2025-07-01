f794e7ae511dcb2f60d7eca69e4b5a46cbce662f
PurpleWizard07 <purplewizard0709@gmail.com>
2025-07-01 12:41:30 +0530

    Time: 7 ms (21.15%), Space: 9.9 MB (26.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,44 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(), s.end()); 
-       sort(t.begin(), t.end()); 
-       if(s==t) return true;
-       else return false;
+        unordered_map<char, int> strS;
+        unordered_map<char, int> strT;
+        for(char c: s) {
+            strS[c]++;
+        }
+        for(char c: t) {
+            strT[c]++;
+        }
+        if(strS == strT) return true;
+        return false;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//        vector<int> freq(26, 0);
+//        for(int i=0; i<s.size(); i++) {
+//         freq[s[i] - 'a']++;
+//         freq[t[i] - 'a']--;
+//        }
+//        for(int i=0; i<freq.size(); i++) {
+//         if(freq[i] != 0) return false;
+//        }
+//        return true;
+//     }
+// };
+
+
+
+
+// // class Solution {
+// // public:
+// //     bool isAnagram(string s, string t) {
+// //        sort(s.begin(), s.end()); 
+// //        sort(t.begin(), t.end()); 
+// //        if(s==t) return true;
+// //        else return false;
+// //     }
+// // };
