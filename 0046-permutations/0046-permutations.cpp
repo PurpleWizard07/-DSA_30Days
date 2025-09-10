e62d4f4169487b138945cc8295d9b8f3304e8023
PurpleWizard07 <purplewizard0709@gmail.com>
2025-09-10 16:08:09 +0530

    Time: 1 ms (42.65%), Space: 11 MB (25.33%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,25 +2,52 @@ class Solution {
     vector<vector<int>> res;
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<bool> pick(nums.size(), false);
-        vector<int> perm;
-        backtrack(perm, nums, pick);
+        backtrack(nums, 0);
         return res;
     }
 
-    void backtrack(vector<int>& perm, vector<int>& nums, vector<bool>& pick) {
-        if (perm.size() == nums.size()) {
-            res.push_back(perm);
+    void backtrack(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            res.push_back(nums);
             return;
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (!pick[i]) {
-                perm.push_back(nums[i]);
-                pick[i] = true;
-                backtrack(perm, nums, pick);
-                perm.pop_back();
-                pick[i] = false;
-            }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            backtrack(nums, idx + 1);
+            swap(nums[idx], nums[i]);
         }
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+//     vector<vector<int>> res;
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<bool> pick(nums.size(), false);
+//         vector<int> perm;
+//         backtrack(perm, nums, pick);
+//         return res;
+//     }
+
+//     void backtrack(vector<int>& perm, vector<int>& nums, vector<bool>& pick) {
+//         if (perm.size() == nums.size()) {
+//             res.push_back(perm);
+//             return;
+//         }
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (!pick[i]) {
+//                 perm.push_back(nums[i]);
+//                 pick[i] = true;
+//                 backtrack(perm, nums, pick);
+//                 perm.pop_back();
+//                 pick[i] = false;
+//             }
+//         }
+//     }
+// };
